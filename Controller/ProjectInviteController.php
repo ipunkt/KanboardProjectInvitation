bacfb1916764156fe6bebf13f284eb6a6ec8cd3a
Hussein <hk@ipunkt.biz>
2019-10-22 10:54:21 +0200

    main Controller für Proj.
    #1

---
+++ Controller/ProjectInviteController.php
@@ -0,0 +1,25 @@
+<?php
+//
+//namespace Kanboard\Plugin\ProjectInvitation\Controller;
+//
+//
+//use Kanboard\Controller\BaseController;
+//
+//
+//class ProjectInvitationController extends BaseController
+//{
+////    /**
+////     * Display the ASF settings page
+////     *
+////     * @access public
+////     */
+////    public function index()
+////    {
+////        $this->response->html($this->helper->layout->config('commentactions:config/comment-actions-settings', array(
+////            'db_size' => $this->configModel->getDatabaseSize(),
+////            'db_version' => $this->db->getDriver()->getDatabaseVersion(),
+////            'user_agent' => $this->request->getServerVariable('HTTP_USER_AGENT'),
+////            'title' => 'Settings'.' &gt; '.'comment actions',
+////        )));
+////    }
+//}
