8747a8f8e02fe275547b75b0a43079dacc5415ee
andrei <av@ipunkt.biz>
2019-10-23 11:10:21 +0200

    check if user's email is in DB exists

--- Controller/ProjectInviteController.php
+++ Controller/ProjectInviteController.php
@@ -13,14 +13,13 @@ class ProjectInviteController extends BaseController
     public function inviteUser()
     {
         $values = $this->request->getValues();
+        $user = $this->userModel->getByEmail($values['email']);
 
-        if (!empty($values['email'])) {
+        if ($user['email'] !== $values['email']) {
             $this->inviteModel->createInvites(array($values['email']), $values['project_id']);
-
             $this->flash->success(t('Invitation to User has been sent successfully.'));
-
         } else {
-            $this->flash->failure(t('User is already invited.'));
+            $this->flash->failure(t('User is already registered.'));
         }
 
         $url = 'project/' . $values['project_id'] . '/permissions';
