0bc1d86c5f9e540a179c5c2a4b5eb133f19dd995
andrei <av@ipunkt.biz>
2019-10-25 14:39:43 +0200

    ru translation

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'Plugin is used to invite new users to actual Project by typing user\'s email.' => 'Плагин используется для приглаешения нового пользователя в проект путём отсылки эл. почты.',
+    'Enter one email address per invite.' => 'Введите один адрес эл. почты для одного приглашения.',
+    'Enter user email...' => 'Введите эл. почту...',
+    'Invite User' => 'Пригласить пользователя',
+    'Invite' => 'Пригласить',
+    'Invitation to user has been sent successfully.' => 'приглашение пользователю успешно выслано',
+    'User is already registered.' => 'Пользователь уже зарегистрирован',
+    'You can add only one email per one invitation.' => 'Вы можете обавить только одного адресата.',
+);
